67b9045a1f875298fa8220457b020dbfc4aaa43e
Tony Smith <smittytone@icloud.com>
2021-01-12 16:51:08 +0000

    Route output correctly

--- utitool/main.swift
+++ utitool/main.swift
@@ -31,7 +31,7 @@ import Foundation
 // MARK: - Constants
 
 let STD_ERR = FileHandle.standardError
-let STD_IN = FileHandle.standardInput
+let STD_OUT = FileHandle.standardOutput
 
 // TTY formatting
 let RED = "\u{001B}[31m"
@@ -100,9 +100,23 @@ func writeToStderr(_ message: String) {
 
     // Write errors and other messages to stderr
 
-    let messageAsString = message + "\r\n"
-    if let messageAsData: Data = messageAsString.data(using: .utf8) {
-        STD_ERR.write(messageAsData)
+    writeTo(STD_ERR, message)
+}
+
+
+func writeToStdout(_ message: String) {
+
+    // Write errors and other messages to stderr
+
+    writeTo(STD_OUT, message)
+}
+
+
+func writeTo(_ fileHandle: FileHandle, _ text: String) {
+    
+    // Write text to the specified channel
+    if let textAsData: Data = (text  + "\r\n").data(using: .utf8) {
+        fileHandle.write(textAsData)
     }
 }
 
@@ -113,9 +127,15 @@ func showHelp() {
 
     showHeader()
 
-    writeToStderr("\nA macOS tool to reveal a file’s Uniform Type Identifier (UTI).")
-    writeToStderr("Copyright 2021, Tony Smith (@smittytone). Source code available under the MIT licence.\r\n")
-    writeToStderr(BOLD + "USAGE" + RESET + "\n    utitool [path 1] [path 2] [path " + ITALIC + "n" + RESET + "]")
+    report("\nA macOS tool to reveal a file’s Uniform Type Identifier (UTI).")
+    report("Copyright 2021, Tony Smith (@smittytone). Source code available under the MIT licence.\r\n")
+    report(BOLD + "USAGE" + RESET + "\n    utitool [path 1] [path 2] ... [path " + ITALIC + "n" + RESET + "]\r\n")
+    report(BOLD + "EXAMPLES" + RESET)
+    report("    utitool *                 -- Get data for all the files in the working directory.")
+    report("    utitool text.md           -- Get data for a named file in the working directory.")
+    report("    utitool text1.md text2.md -- Get data for named files in the working directory.")
+    report("    utitool /User/me/text1.md -- Get data for any named file.")
+    report("    utitool ../text1.md       -- Get data for a named file in the parent directory.")
 }
 
 
@@ -126,37 +146,64 @@ func showHeader() {
     let version: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
     let build: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
     let name:String = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as! String
-    writeToStderr("\(name) \(version) (\(build))")
+    report("\(name) \(version) (\(build))")
 }
 
 
 // MARK: - Runtime Start
 
+// Trap CTRL-C
 signal(SIGINT) {
-    theSignal in writeToStderr("\(BSP)\(BSP)\rutitool interrupted -- halting")
+    theSignal in report("\(BSP)\(BSP)\rutitool interrupted -- halting")
     exit(EXIT_FAILURE)
 }
 
+// Get the command line args...
 var args = CommandLine.arguments
 
+// ...and process them
 if args.count == 1 {
+    // No user args? Just show the help info
     showHelp()
 } else {
+    // Get a file manager
+    let fm = FileManager.default
+    var count: UInt = 0
+    
     // Convert passed paths to URL
     for i: Int in 1..<args.count {
         
         let path = getFullPath(args[i])
-        let url: URL = URL.init(fileURLWithPath: path, isDirectory: false)
-
-        if let uti = url.typeIdentifier {
-            report("UTI for \(path) is: \(uti)")
-        } else {
-            reportError("Could not get UTI for \(path)")
+        var isDir: ObjCBool = false
+        
+        // Check that we're only dealing with files
+        if fm.fileExists(atPath: path, isDirectory: &isDir) {
+            if !isDir.boolValue {
+                // Make a URL from the path
+                let url: URL = URL.init(fileURLWithPath: path, isDirectory: false)
+                
+                // And output the UTI if we can
+                if let uti = url.typeIdentifier {
+                    writeToStdout("UTI for \(path): \(uti)")
+                } else {
+                    reportError("Could not get UTI for \(path)")
+                }
+                
+                // Tally the number of files reported on
+                count += 1
+            }
         }
+    }
+    
+    // No reported files? Issue a warning
+    if count == 0 {
+        report("No files specified or present")
     }
 }
 
+// Exit gracefully
 exit(EXIT_SUCCESS)
+
 
 // MARK: - URL Extension
 
